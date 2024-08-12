73035ac19f4e661ff5292e4e5b13dd48c9ae2489
Mausumi  Ghadei <117946464+Mausumi134@users.noreply.github.com>
2024-08-12 20:08:38 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://flashcard-tool-1e9b.onrender.com';
+const apiUrl = 'https://flashcard-tool-1.onrender.com';
 
 export const getFlashcards = async () => {
   const response = await axios.get(`${apiUrl}/flashcards`);
